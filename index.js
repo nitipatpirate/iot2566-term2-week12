ed3ca22dce939f62897a42580b7c6ec17b169719
Jimmyseesang <jimmyseesang@gmail.com>
2023-01-05 17:36:14 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv')
 const express = require('express')
-// const path = require('path')
+//const path = require('path')
 const app = express()
 
 //set path to .env
@@ -9,17 +9,22 @@ const PORT = process.env.PORT || 4000
 
 //http://localhost:5000/
 app.get('/',(req,res)=>{
-    return res.status(200).sendFile(path.join(__dirname+ '/pages/index.html'))
+    return res.status(200).sendFile(path.join(__dirname+ 'pages/index.html'))
 })
 
 //http://localhost:5000/about
 app.get('/about',(req,res)=>{
-    return res.status(200).sendFile(path.join(__dirname+ '/pages/about.html'))
+    return res.status(200).sendFile(path.join(__dirname+ 'pages/about.html'))
 })
 
 //http://localhost:5000/profile
 app.get('/profile',(req,res)=>{
-    return res.status(200).sendFile(path.join(__dirname+ '/pages/profile.html'))
+    return res.status(200).sendFile(path.join(__dirname+ 'pages/profile.html'))
+})
+
+//404 Not Found page
+app.use((req,res,next)=>{
+    return res.status(404).sendFile(path.join(__dirname+ 'pages/'))
 })
 
 app.listen(PORT,()=>{
